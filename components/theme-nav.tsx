b7ae642b7925186f2664f567e698682242beb2df
choidy180 <choidy180@naver.com>
2022-11-16 15:01:49 +0900

    commit

--- components/theme-nav.tsx
+++ components/theme-nav.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { BsPlusLg, BsSun, BsMoon } from 'react-icons/bs';
+import { BsSun, BsMoon } from 'react-icons/bs';
 import { useRecoilState, useSetRecoilState } from 'recoil';
 import { isThemeAtom } from '../recoil/theme'
 import { BsPalette } from 'react-icons/bs'
@@ -18,17 +18,23 @@ const ThemeNavigation = ({changeTheme}:any) => {
     return (
         <Container className={view ? '' : 'none'}>
             <Title>테마 설정</Title>
-            <BsPlusLg className="cancel" onClick={()=> setView(false)}/>
+            <svg xmlns="http://www.w3.org/2000/svg" onClick={()=> setView(false)} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="cancel">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
             <LightDarkTheme color={themeState[0]}>
                 <div onClick={()=>changeTheme(true)}>
-                    <BsSun className="sun"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="sun">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+                    </svg>
                 </div>
                 <div onClick={()=>changeTheme(false)}>
-                    <BsMoon/>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="moon">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+                    </svg>
                 </div>
             </LightDarkTheme>
             <PointTheme>
-                <h2>Color Ficker</h2>
+                <h2 onClick={()=>console.log(themeState[0])}>Color Ficker</h2>
                 <div className="container">
                     <ColorContent onClick={()=>useColorSet('#f53b57')} color={'#f53b57'}/>
                     <ColorContent onClick={()=>useColorSet('#3c40c6')} color={'#3c40c6'}/>
@@ -37,7 +43,9 @@ const ThemeNavigation = ({changeTheme}:any) => {
             </PointTheme>
             <ButtonBox color={themeState[0]} className={view ? 'none' : ''} onClick={()=> setView((e) => !e)}>
                 <button>
-                    <BsPalette/>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.098 19.902a3.75 3.75 0 005.304 0l6.401-6.402M6.75 21A3.75 3.75 0 013 17.25V4.125C3 3.504 3.504 3 4.125 3h5.25c.621 0 1.125.504 1.125 1.125v4.072M6.75 21a3.75 3.75 0 003.75-3.75V8.197M6.75 21h13.125c.621 0 1.125-.504 1.125-1.125v-5.25c0-.621-.504-1.125-1.125-1.125h-4.072M10.5 8.197l2.88-2.88c.438-.439 1.15-.439 1.59 0l3.712 3.713c.44.44.44 1.152 0 1.59l-2.879 2.88M6.75 17.25h.008v.008H6.75v-.008z" />
+                </svg>
                 </button>
             </ButtonBox>
         </Container>
@@ -61,10 +69,10 @@ const Container = styled.div`
     padding: 14px;
     .cancel{
         position: absolute;
+        top: 12px;
         right: 12px;
-        width: 20px;
-        height: 20px;
-        transform: rotate(45deg);
+        width: 28px;
+        height: 28px;
         cursor: pointer;
     }
     &.none{
@@ -96,12 +104,12 @@ const LightDarkTheme = styled.div`
         cursor: pointer;
         transition: all .15s ease-in-out;
         svg {
-            width: 22px;
-            height: 22px;
+            width: 28px;
+            height: 28px;
         }
         .sun {
-            width: 26px;
-            height: 26px;
+            width: 32px;
+            height: 32px;
         }
     }
 `
